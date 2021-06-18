56a9d60df0e76257d1364b1521454b5afd13ee69
Frostyyxo <50382570+Frostyyxo@users.noreply.github.com>
2021-06-19 09:21:40 +1200

    Create ConvBlock class

---
+++ models/mobilenetv1.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels,
+                              out_channels=out_channels,
+                              kernel_size=kernel_size,
+                              stride=stride)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
