62faa0830e26870b902fe39f3d20fec25daef2aa
Frostyyxo <vandijcksm@gmail.com>
2021-09-04 21:48:46 +1200

    mobilenet class and function

--- models/cnn/mobilenetv2.py
+++ models/cnn/mobilenetv2.py
@@ -68,7 +68,7 @@ class InvertedResidualBlock(nn.Module):
         else:
             return self.features(x)
 
-class MobileNetV2(nn.Module):
+class Mobilenetv2(nn.Module):
     def __init__(self, 
                  num_classes=1000, 
                  width_multi=1.0, 
@@ -76,4 +76,67 @@ class MobileNetV2(nn.Module):
                  round_nearest=8, 
                  block=None, 
                  norm_layer=None):
-        super(MobileNetV2, self).__init__()
+        super(Mobilenetv2, self).__init__()
+        input_channel = 32
+        last_channel = 1280
+
+        if inverted_residual_setting is None:
+            inverted_residual_setting = [
+                # expansion factor, output channels, repeating number, stride
+                [1, 16, 1, 1],
+                [6, 24, 2, 2],
+                [6, 32, 3, 2],
+                [6, 64, 4, 2],
+                [6, 96, 3, 1],
+                [6, 160, 3, 2],
+                [6, 320, 1, 1],
+            ]
+
+        #Check inverted_residual_setting
+        if len(inverted_residual_setting) == 0 or len(inverted_residual_setting[0]) != 4:
+            raise ValueError("inverted_residual_setting should be non-empty "
+                             "or a 4-element list, got {}".format(inverted_residual_setting))
+
+        #first layer
+        input_channel = _make_divisible(input_channel * width_multi, round_nearest)
+        self.last_channel = _make_divisible(last_channel * max(1.0, width_multi), round_nearest)
+        features = [ConvLayer(3, input_channel, stride=2, norm_layer=norm_layer)]
+        #inverted residual blocks
+        for t, c, n, s, in inverted_residual_setting:
+            output_channel = _make_divisible(c * width_multi, round_nearest)
+            for i in range(n):
+                stride = s if i == 0 else 1
+                features.append(InvertedResidualBlock(input_channel, 
+                                      output_channel, 
+                                      stride, 
+                                      expand_ratio=t, 
+                                      norm_layer=norm_layer))
+                input_channel = output_channel
+
+        #last layers
+        features.append(ConvLayer(input_channel, self.last_channel, kernel_size=1, norm_layer=norm_layer))
+
+        self.features = nn.Sequential(*features)
+
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.dropout = nn.Dropout(p=0.2)
+        self.fc1 = nn.Linear(self.last_channel, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.pool(x)
+        x = self.dropout(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        return x
+
+def MobileNetV2(num_classes=1000, 
+                 inverted_residual_setting=None, 
+                 round_nearest=8, 
+                 block=None, 
+                 norm_layer=None):
+    return Mobilenetv2(num_classes, 
+                       inverted_residual_setting,
+                       round_nearest, 
+                       block, 
+                       norm_layer)
