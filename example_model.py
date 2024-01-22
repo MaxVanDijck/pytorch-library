11f60f5e506a8c5d114865c05605260cfa0f84b2
MaxVanDijck <vandijcksm@gmail.com>
2024-01-22 16:59:46 +1300

    imagenette

--- example_model.py
+++ example_model.py
@@ -1,10 +1,16 @@
 from transformers import PreTrainedModel, PretrainedConfig
 import torch
 from timm.models.resnet import BasicBlock, Bottleneck, ResNet
+from models.cnn.alexnet import AlexNet
+from models.cnn.resnet import ResNet50
 import pytorch_lightning as pl
 import transformers
 
 from deepspeed.ops.adam import DeepSpeedCPUAdam
+
+
+
+from fastai.vision.all import xse_resnext50, Mish, MaxPool
 
 BLOCK_MAPPING = {"basic": BasicBlock, "bottleneck": Bottleneck}
 class Config(PretrainedConfig):
@@ -21,17 +27,17 @@ class Config(PretrainedConfig):
 class Model(PreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
-        block_layer = BLOCK_MAPPING[config.block_type]
-        self.model = ResNet(
-            block_layer,
-            [3, 3, 4, 3],
-            num_classes=200,
-        )
+        self.model = xse_resnext50(n_out=10, act_cls=Mish, sa=1, sym=0, pool=MaxPool)
 
     def forward(self, tensor, labels=None):
         logits = self.model(tensor.to(self.device))
         if labels is not None:
-            loss = torch.nn.functional.cross_entropy(logits, labels.to(self.device))
+            loss = torch.nn.functional.cross_entropy(logits, labels.to(self.device), label_smoothing=0.1)
             return {"loss": loss, "logits": logits}
         return {"logits": logits}
+
+
+
+
+
 
