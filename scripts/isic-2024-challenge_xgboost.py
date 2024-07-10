29f68189a6640774a691b1bdf14ab9026a1ea213
MaxVanDijck <vandijcksm@gmail.com>
2024-07-10 21:37:56 +1200

    almost finished xgboost script

--- scripts/isic-2024-challenge_xgboost.py
+++ scripts/isic-2024-challenge_xgboost.py
@@ -2,11 +2,14 @@ import ray
 from ray.train import ScalingConfig
 from ray.train.xgboost import XGBoostTrainer
 import polars as pl
+import pandas as pd
+import xgboost as xgb
+
+
 
 if __name__ == "__main__":
-    ray.init()
     df = pl.read_csv("../data/isic-2024-challenge/train-metadata.csv")
-    df = df.drop([
+    cols = [
         "isic_id",
         "sex", # potentially useful
         "anatom_site_general",
@@ -31,7 +34,9 @@ if __name__ == "__main__":
         "tbp_lv_location",
         "tbp_lv_location_simple",
         "age_approx",
-    ])
+
+    ]
+    df = df.drop(cols)
 
     patient_ids = df["patient_id"].unique().to_list()
 
@@ -49,16 +54,21 @@ if __name__ == "__main__":
     test_ds = ray.data.read_parquet("../data/isic-2024-challenge/test_tabular.parquet")
 
 
-
     trainer = XGBoostTrainer(
+        scaling_config=ScalingConfig(
+            # Number of workers to use for data parallelism.
+            num_workers=1,
+            # Whether to use GPU acceleration. Set to True to schedule GPU workers.
+            use_gpu=False,
+        ),
         label_column="target",
-        num_boost_round=20,
+        num_boost_round=50,
         params={
             # XGBoost specific params (see the `xgboost.train` API reference)
             "objective": "binary:logistic",
             # uncomment this and set `use_gpu=True` to use GPU for training
             # "tree_method": "gpu_hist",
-            "eval_metric": ["logloss", "error"],
+            "eval_metric": ["auc"],
         },
         datasets={"train": train_ds, "valid": test_ds},
         # If running in a multi-node cluster, this is where you
@@ -68,4 +78,30 @@ if __name__ == "__main__":
     )
     result = trainer.fit()
     print(result.metrics)
+    
+    checkpoint = result.checkpoint
+
+    class Predictor:
+        def __init__(self):
+            self.model = XGBoostTrainer.get_model(checkpoint)
+
+        def __call__(self, df: pd.DataFrame) -> pd.DataFrame:
+            orig_df = df
+            if df.empty:
+                return df
+
+            df = df.drop(["isic_id"], axis=1)
+
+            x = xgb.DMatrix(df)
+            orig_df["target"] = self.model.predict(x).tolist()
+            orig_df = orig_df.drop(list(set(orig_df.columns) - set(["target", "isic_id"])), axis=1)
+            return orig_df
+
+    test_csv = pd.read_csv("../data/isic-2024-challenge/test-metadata.csv")
+    test_csv = test_csv.drop([col for col in list(set(test_csv.columns) - set(train_df.columns) - set(["isic_id"])) + ["target"] if col in test_csv.columns], axis=1)
+
+    inferenced_dataset = (ray.data.from_pandas(test_csv)
+        .map_batches(Predictor, batch_size=2048, batch_format="pandas", concurrency=1)).to_pandas()
+
     breakpoint()
+
